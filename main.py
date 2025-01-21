f9aa8aad5a5572298d7c2c9685b071f2da1b437f
ededd9 <edhur9@gmail.com>
2025-01-20 20:05:48 -0500

    modified game loop to use Groups

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pygame
 from constants import *
-
-
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	#grouping
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable,drawable)
+	
+	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+	dt = 0
+
 	while(True):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		for obj in updatable:
+			obj.update(dt)
 		screen.fill("black")
+		for obj in drawable:
+			obj.draw(screen)
 		pygame.display.flip()
-		
-	print("Starting asteroids!")
-	print("Screen width:",SCREEN_WIDTH)
-	print("Screen height:",SCREEN_HEIGHT )
+		# 60 fps framerate
+		dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
 	main()
