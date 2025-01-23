3f0c4646071d91ed4d312a2f4df2b19411a04ed8
ededd9 <edhur9@gmail.com>
2025-01-22 19:00:57 -0500

    added logic for collisions between asteroids, player, and bullet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
@@ -9,10 +12,15 @@ def main():
 	#grouping
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	playershots = pygame.sprite.Group()
 	Player.containers = (updatable,drawable)
-	
+	Asteroid.containers = (asteroids, updatable,drawable)
+	AsteroidField.containers = (updatable)
+	Shot.containers = (playershots, updatable,drawable)
 	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 	dt = 0
+	asteroidfield = AsteroidField()
 
 	while(True):
 		for event in pygame.event.get():
@@ -20,6 +28,14 @@ def main():
 				return
 		for obj in updatable:
 			obj.update(dt)
+		for ast in asteroids:
+			if player.collisions(ast):
+				print("Game over!")
+				exit()
+			for bullet in playershots:
+				if bullet.collisions(ast):
+					bullet.kill()
+					ast.split()
 		screen.fill("black")
 		for obj in drawable:
 			obj.draw(screen)
